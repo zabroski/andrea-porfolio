6c824522ff3316370fbf2f14c79de2c8f0556ef5
zabroski <35234595+zabroski@users.noreply.github.com>
2025-10-27 13:25:58 -0400

    first commit with header cpomponent

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header/header.js";
+// import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* <Routes></Routes> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <header>Hello Header</header>;
+};
+
+export default Header;
